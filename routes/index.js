5bb8760c3dfe1827f1b08dc7f52666e5732878e3
Dibel <linyizyl@pku.edu.cn>
2014-05-23 10:59:32 +0800

    small changes

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,10 @@ router.get('/home', function(req, res) {
   res.render('home', { title: 'WorkOne—高效的团队协作工具', id: 'home', name: name });
 });
 
+router.get('/user', function(req, res) {
+   res.render('user', {title: '个人主页', id: 'user', name: name});
+});
+
 router.get('/about', function(req, res) {
     res.render('about', { title: '关于WorkOne', id: 'about', name: name });
 });
--- routes/login.js
+++ routes/login.js
@@ -10,4 +10,12 @@ router.get('/', function(req, res) {
     res.render('login', { title: '登录到WorkOne', id: 'login', name: name });
 });
 
+router.post('/', function(req, res) {
+    var name = res.body.username;
+    var password = res.body.password;
+    if(name != "" && password != "") {
+        return router.redirect('/home');
+    }
+});
+
 module.exports = router;
